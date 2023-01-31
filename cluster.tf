ce6ad6ee18c6b9f9cc9c47438729995278fb89cd
Abraham Leal <leal901@gmail.com>
2023-01-31 14:36:34 -0800

    Establish Env

---
+++ cluster.tf
@@ -0,0 +1,49 @@
+resource "confluent_environment" "Dev" {
+  display_name = "Development"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "confluent_kafka_cluster" "dedicated" {
+  display_name = "dedicated_kafka_cluster"
+  availability = "MULTI_ZONE"
+  cloud        = "AWS"
+  region       = "us-east-2"
+  dedicated {
+    cku = 2
+  }
+
+  environment {
+    id = confluent_environment.Dev.id
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "confluent_api_key" "app-manager-kafka-api-key" {
+  display_name = "app-manager-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'app-manager' service account"
+  owner {
+    id          = confluent_service_account.app-manager.id
+    api_version = confluent_service_account.app-manager.api_version
+    kind        = confluent_service_account.app-manager.kind
+  }
+
+  managed_resource {
+    id          = confluent_kafka_cluster.dedicated.id
+    api_version = confluent_kafka_cluster.dedicated.api_version
+    kind        = confluent_kafka_cluster.dedicated.kind
+
+    environment {
+      id = confluent_environment.Dev.id
+    }
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
--- providers.tf
+++ providers.tf
@@ -8,5 +8,18 @@ terraform {
 }
 
 provider "confluent" {
-  # Configuration options
+  cloud_api_key    = var.confluent_cloud_api_key 
+  cloud_api_secret = var.confluent_cloud_api_secret
+}
+
+
+resource "confluent_service_account" "app-manager" {
+  display_name = "app-manager"
+  description  = "Service Account for orders app"
+}
+
+resource "confluent_role_binding" "app-manager-rb" {
+  principal   = "User:${confluent_service_account.app-manager.id}"
+  role_name   = "EnvironmentAdmin"
+  crn_pattern = confluent_environment.Dev.resource_name
 }

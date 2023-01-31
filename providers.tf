16c1068d9b54235b1476bc22edde1f940311bedf
Alex Schmid <aschmid13@gmail.com>
2023-01-31 14:17:04 -0800

    framework

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    confluent = {
+      source = "confluentinc/confluent"
+      version = "1.28.0"
+    }
+  }
+}
+
+provider "confluent" {
+  # Configuration options
+}
